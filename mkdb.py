ad34155ffa943f8c4036fbd1244ea512dff1e090
Emil Vatai <emil.vatai@gmail.com>
2014-12-14 22:32:55 +0100

    insert one of each

--- mkdb.py
+++ mkdb.py
@@ -1,10 +1,18 @@
-from elly import db,Cls,User
+from datetime import datetime
+from elly import db,Cls,User,Problem,Solution
 
 db.create_all()
 
 cls = Cls('proba')
-user = User('vatai', 'Vatai','Emil', 'pass123', cls)
-
 db.session.add(cls)
+
+user = User('vatai', 'Vatai','Emil', 'pass123', cls)
 db.session.add(user)
+
+problem = Problem('irj egy programot',cls)
+db.session.add(problem)
+
+solution = Solution(problem, user, datetime.utcnow())
+db.session.add(solution)
+
 db.session.commit()
