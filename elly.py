981127ff6e268ba2d3efc9fe37426fbf14871344
Emil Vatai <emil.vatai@gmail.com>
2014-12-15 08:11:16 +0100

    upload seems to work

--- elly.py
+++ elly.py
@@ -1,16 +1,27 @@
 from datetime import datetime
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import secure_filename
+import os 
+import glob
+
 
 DATABASE="bolyai.db"
 DEBUG=True
+SECRET_KEY = 'development key'
+UPLOAD_FOLDER = 'pas'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','lpr','pas'])
 print('Elly started')
+
 
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DATABASE
 db = SQLAlchemy(app)
+
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +42,8 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+
+
 class Cls(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     classname = db.Column(db.String(10), unique=True)
@@ -40,6 +53,8 @@ class Cls(db.Model):
 
     def __repr__(self):
         return '<Class %r>' % self.classname
+
+
 
 class Problem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -54,6 +69,8 @@ class Problem(db.Model):
 
     def __repr__(self):
         return '<Problem %r>' % self.username
+
+
 
 class Solution(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -71,11 +88,61 @@ class Solution(db.Model):
     def __repr__(self):
         return '<Solution %r>' % self.username
 
-# import hashlib; hashlib.sha224('elteik').hexdigest()
 
-@app.route('/')
-def index():
-    return render_template('t1.html')
+
+# import hashlib; hashlib.sha224('elteik').hexdigest()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        q = User.query.filter_by(username=username).first()
+        if not q:
+            error = 'Invalid username'
+        elif q.password != password:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_problem'))
+
+    return render_template('login.html',error=error)
+
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def show_problem():
+    if request.method == 'POST':
+        file = request.files['file']
+        ip = request.remote_addr
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], ip+filename))
+            return redirect(url_for('show_entries',
+                                    filename='finished'))
+    return render_template('problem.html')
+
+
+
+@app.route('/show_entries')
+def show_entries():
+    entries = glob.glob('pas/*')
+    print entries
+    return render_template('show_entries.html', entries=entries)
 
 if __name__ == '__main__':
     app.run()
