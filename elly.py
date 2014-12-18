56146ded04c9e56e00e3ada6fcd86965d9c472fa
Emil Vatai <emil.vatai@gmail.com>
2014-12-18 22:23:54 +0100

    For one problem it works... :-/

--- elly.py
+++ elly.py
@@ -2,15 +2,16 @@ from datetime import datetime
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from werkzeug import secure_filename
-import os 
-import glob
+from subprocess import check_output,STDOUT,CalledProcessError 
+import os,glob,re,string,random,tempfile
 
+COMPILER = "fpc"
 
 DATABASE="bolyai.db"
-DEBUG=False
+DEBUG=True
 SECRET_KEY = 'development key'
-UPLOAD_FOLDER = 'pas'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','lpr','pas'])
+UPLOAD_FOLDER = 'new'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','lpr','pas', 'pl'])
 print('Elly started')
 
 
@@ -68,7 +69,7 @@ class Problem(db.Model):
         self.cls = cls
 
     def __repr__(self):
-        return '<Problem %r>' % self.username
+        return '<Problem %r>' % self.id
 
 
 
@@ -91,9 +92,10 @@ class Solution(db.Model):
 
 
 # import hashlib; hashlib.sha224('elteik').hexdigest()
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
+    session['logged_in'] = False
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -104,8 +106,12 @@ def login():
             error = 'Invalid password'
         else:
             session['logged_in'] = True
+            session['username'] = q.username
+            session['firstname'] = q.firstname
+            session['lastname'] = q.lastname
+            session['cls'] = q.cls.classname
             flash('You were logged in')
-            return redirect(url_for('show_problem'))
+            return redirect(url_for('problem_select'))
 
     return render_template('login.html',error=error)
 
@@ -124,25 +130,143 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/', methods=['GET', 'POST'])
+
+def save_file(file):
+    d = {}
+    d['ip']=request.remote_addr
+    d['updir']=session['cls']
+    d['lastname']=session['lastname']
+    d['firstname']=session['firstname']
+    filename = '{updir}/{ip}-{lastname}-{firstname}.pas'
+    filename = filename.format(**d)
+    session['filename'] = filename
+    #filename = secure_filename(filename)
+    if not os.path.isdir(d['updir']):
+        os.mkdir(d['updir'])
+    file.save(filename)
+    
+
+
+@app.route('/show_problem', methods=['GET', 'POST'])
 def show_problem():
     if request.method == 'POST':
         file = request.files['file']
-        ip = request.remote_addr
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], ip+filename))
-            return redirect(url_for('show_entries',
-                                    filename='finished'))
-    return render_template('problem.html')
+            save_file(file)
+            return redirect(url_for('check_solution'))
+    
+    id=request.args.get('id', '')
+    session['problem_id']=id
+    compilesolution(id)
+
+    q = Problem.query.filter_by(id=id).first()
+    problemtext = q.problemtext
+    return render_template('problem.html',problemtext=problemtext)
 
 
 
-@app.route('/show_entries')
-def show_entries():
-    entries = glob.glob('pas/*')
-    print entries
-    return render_template('show_entries.html', entries=entries)
+@app.route('/check_solution')
+def check_solution():
+    # TODO error + errmsg
+    error=None
+    filename = session['filename']
+
+    (rv,output)=compile(filename)
+    
+    if rv != 0:
+        error = 'A program nem fordult le! ' + output
+        return render_template('check_result.html',error=error)
+    
+    error = runtest()
+    
+    if error == None:
+        save_solution()
+    return render_template('check_result.html',error=error)
+
+
+def save_solution():
+    problem = Problem.query.filter_by(id=session['problem_id']).first()
+    user = User.query.filter_by(username=session['username']).first()
+    sol = Solution(problem,user,datetime.utcnow())
+    db.session.add(sol)
+    db.session.commit()
+
+
+
+def generate_random_file(n=80,nl=10,contents=string.printable):
+    FNLENGTH = 8
+
+    # rnl = (random.randint(0,nl),random.randint(0,nl))
+    # rnl = (min(rnl),max(rnl))
+    # rn = (random.randint(0,n),random.randint(0,n))
+    # rn = (min(rn),max(rn))
+
+    (f,filename) = tempfile.mkstemp(text=True)
+    os.close(f)
+    f=open(filename,'w')
+    for j in range(nl):
+        for i in range(n):
+            f.write(random.choice(contents))
+        if nl!=0: f.write('\n')
+        
+    f.close()
+    return filename
+
+
+    
+def runtest():
+    exename = session['filename'].split('.pas')[0]
+    tf=tempfile.SpooledTemporaryFile()
+    fn = generate_random_file()
+    tf.write(bytes(fn,'UTF-8'))
+    tf.seek(0)
+    solution_output = check_output('./'+session['problem_id'],stdin=tf,stderr=STDOUT)
+    tf.seek(0)
+    rv = None
+    try:
+        output = check_output([exename],stdin=tf,stderr=STDOUT)
+    except CalledProcessError as e:
+        rv = 'A program nem futot le!\n{}'.format(e.output)
+    finally:
+        tf.close()
+    if output != solution_output:
+        t = 'A program nem a megfelelő eredményt adta.'
+        t = t + 'Várt:{}Kapott:{}'
+        rv = t.format(output.decode('UTF-8'),solution_output.decode('UTF-8'))
+    os.remove(fn)
+    return rv
+
+
+# @app.route('/show_entries')
+# def show_entries():
+#     pattern =os.path.join(app.config['UPLOAD_FOLDER'],'*')
+#     entries = glob.glob(pattern)
+#     entries = map(lambda x : re.sub(app.config['UPLOAD_FOLDER']+'/?','',x), entries)
+#     return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/problem_select')
+def problem_select():
+    if session['logged_in'] == True:
+        q = Problem.query.all()
+        ids = map(lambda x : x.id, q)
+        return render_template('problem_select.html', ids=ids)
+    return redirect(url_for('login'))
+
+def compile(filename):
+    print('Compiling {}'.format(filename))
+    cmd = [COMPILER,filename]
+    try:
+        out = check_output(cmd,stderr=STDOUT)
+        return (0,out.decode('UTF-8'))
+    except CalledProcessError as e:
+        return (e.returncode,e.output.decode('UTF-8'))
+
+def compilesolution(id):
+    fn = id+'.pas'
+    if not os.path.exists(fn) or not os.access(fn, os.X_OK):
+        compile(fn)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
--- mkdb.py
+++ mkdb.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from elly import db,Cls,User,Problem,Solution
+from unidecode import unidecode
 
 db.create_all()
 
@@ -9,10 +10,42 @@ db.session.add(cls)
 user = User('vatai', 'Vatai','Emil', 'pass123', cls)
 db.session.add(user)
 
-problem = Problem('irj egy programot',cls)
+problem = Problem('''Írj egy programot amely egy integereket tartalmazó fájlból kikeresi a legkisebbet és azt kiírja a képernyőre. Ha a fájl üres akkor ne írjon ki semmit''',cls)
+db.session.add(problem)
+
+problem = Problem('irj még egy programot',cls)
 db.session.add(problem)
 
 solution = Solution(problem, user, datetime.utcnow())
 db.session.add(solution)
+
+db.session.commit()
+
+
+cls = Cls('2m')
+db.session.add(cls)
+
+lista=[['Ali', 'Arszen'],
+   ['Bencsik', 'Blanka'],
+   ['Bozsóki', 'Andor'],
+   ['Bódi', 'Viktor'],
+   ['Csipak', 'Noémi'],
+   ['Fehér', 'Krisztián'],
+   ['Fenyvesi', 'Abigél'],
+   ['Fodor', 'Ádám'],
+   ['Gajda', 'Benedek'],
+   ['Galusz', 'Márton'],
+   ['Guyás', 'Nikoletta'],
+   ['Kovács', 'Noel'],
+   ['Lukács', 'Máté'],
+   ['Szögi', 'Evelin'],
+   ['Toldi', 'Teodóra'],
+   ['Tóth', 'Koppány'],
+   ['Vrbaški', 'Viktor']]
+
+lista=map(lambda x : [unidecode(x[0]).lower(),x[0],x[1],'pass',cls],lista)
+for s in lista:
+    user = User(*s)
+    db.session.add(user)
 
 db.session.commit()
